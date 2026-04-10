b1122cbb7ff542d86bdcdc12ff7721d49a894e3a
wateronenn <anamneung@gmail.com>
2026-04-10 16:35:30 +0700

    edit POST status

--- controllers/hotels.js
+++ controllers/hotels.js
@@ -30,7 +30,7 @@ exports.createHotel = async (req,res,next) => {
     try{
         const hotel = await Hotel.create(req.body)
 
-        res.status(200).json({
+        res.status(201).json({
             success:true,
             data:hotel
         });
