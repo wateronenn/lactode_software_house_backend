b74111342f595e1ec95d56af7b2fdc35126a13ac
wateronenn <anamneung@gmail.com>
2026-04-10 16:15:20 +0700

    feature/createHotel : create API endpoint for hotel & role access control - createHotel controller - routes for hotel - API endpoint for bookings rooms and hotel Closed # 69

--- controllers/hotels.js
+++ controllers/hotels.js
@@ -4,9 +4,60 @@ const Hotel = require('../models/Hotel')
 // @desc    view all hotel
 // @route   GET api/v1/hotels
 // @access  Public
+exports.getManyHotels = async(req,res,next) => {
+
+}
 
 
 
-// @desc    Register user
-// @route   POST /api/v1/auth/register
+// @desc    view single user
+// @route   GET /api/v1/hotels/:id
 // @access  Public
+exports.getSingleHotel = async(req,res,next) => {
+
+}
+
+// @desc    create hotel
+// @route   POST api/v1/hotels
+// @access  admin
+exports.createHotel = async (req,res,next) => {
+    if(req.user.role !== 'admin'){
+        res.status(403).json({
+            success:false,
+            msg:"Not authorized to access this path"
+        })
+    }
+    try{
+        const hotel = await Hotel.create(req.body)
+
+        res.status(200).json({
+            success:true,
+            data:hotel
+        });
+    }catch (err){
+        res.status(400).json({
+            success:false,
+            msg:`Cannot create hotel : {$err}`
+        })
+    }
+};
+
+
+// @desc    update hotel
+// @route   PUT api/v1/hotels/:id
+// @access  admin , hotel owner
+
+
+exports.updateHotel = async(req,res,next) => {
+
+}
+
+
+
+
+// @desc    delete hotel
+// @route   DELETE api/v1/hotels/:id
+// @access  admin
+exports.deleteHotel = async(req,res,next) => {
+
+}
---
+++ controllers/rooms.js
@@ -0,0 +1,21 @@
+const Room = require('../models/Room')
+
+// @desc    view all rooms
+// @route   GET api/v1/hotels/:id/rooms
+// @access  Public
+
+// @desc    view single rooms
+// @route   GET api/v1/hotels/:id/rooms/:id
+// @access  Public
+
+// @desc    create room
+// @route   POST api/v1/hotels/:id/rooms
+// @access  hotel
+
+// @desc    update rooms
+// @route   PUT api/v1/hotels/:id/rooms/:id
+// @access  hotel
+
+// @desc    delete rooms
+// @route   DELETE api/v1/hotels/:id/rooms/:id
+// @access  hotel
