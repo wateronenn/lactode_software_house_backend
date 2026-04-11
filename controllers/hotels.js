4c2f6be808dd387d3592bca3fc0c15bb5364277a
Lin <thitisan3923@gmail.com>
2026-04-11 17:46:42 +0700

    feature delete hotel controller

--- controllers/hotels.js
+++ controllers/hotels.js
@@ -114,7 +114,7 @@ exports.createHotel = async (req,res,next) => {
     }catch (err){
         res.status(400).json({
             success:false,
-            msg:`Cannot create hotel : {$err}`
+            msg:`Cannot create hotel : ${err.message}`
         })
     }
 };
@@ -151,7 +151,7 @@ exports.updateHotel = async(req,res,next) => {
     }catch (err){
         res.status(400).json({
             success:false,
-            msg:`Cannot update hotel : {$err}`
+            msg:`Cannot update hotel : ${err.message}`
         })
     }
 }
@@ -163,7 +163,25 @@ exports.updateHotel = async(req,res,next) => {
 // @route   DELETE api/v1/hotels/:id
 // @access  admin
 exports.deleteHotel = async(req,res,next) => {
+    if(req.user.role !== 'admin'){
+        res.status(403).json({
+            success:false,
+            msg:"Not authorized to access this path"
+        })
+    }
+    try{
+        const hotel = await Hotel.findByIdAndDelete(req.params.id);
 
+        if(!hotel){
+            return res.status(400).json({success:false});
+        }
+        res.status(200).json({success:true,data:{}});
+    }catch (err){
+        res.status(400).json({
+            success:false,
+            msg:`Cannot delete hotel : ${err.message}`
+        })
+    }
 }
 
 
