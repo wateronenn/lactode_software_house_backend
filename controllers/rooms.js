5d04af053143ab4e773b787ba83bc9325da0700b
Ninehua-gh <ninehua49@gmail.com>
2026-04-12 21:58:39 +0700

    feature : create and delete room

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -1,4 +1,6 @@
 const Room = require('../models/Room')
+const Hotel = require('../models/Hotel');
+
 
 // @desc    view all rooms
 // @route   GET api/v1/hotels/:id/rooms
@@ -16,7 +18,7 @@ exports.getSingleRoom = async (req, res) => {
         }
 
         // Make sure the room actually belongs to the hotel in the URL
-        if (room.hotelID._id.toString() !== req.params.hotelID) {
+        if (room.hotelID._id.toString() !== req.params.hotelId) {
             return res.status(400).json({ success: false, message: 'Room does not belong to this hotel' });
         }
 
@@ -29,6 +31,34 @@ exports.getSingleRoom = async (req, res) => {
 // @desc    create room
 // @route   POST api/v1/hotels/:id/rooms
 // @access  hotel
+exports.createRoom = async (req, res) => {
+    try {
+        const hotel = await Hotel.findById(req.params.hotelId);
+        if (!hotel) {
+            return res.status(404).json({ success: false, message: 'Hotel not found' });
+        }
+
+        if (req.user.role === 'hotelOwner' && hotel.ownerID.toString() !== req.user.id.toString()) {
+            return res.status(403).json({ success: false, message: 'Not authorized to create room in this hotel' });
+        }
+
+        req.body.hotelID = req.params.hotelId;
+        const room = await Room.create(req.body);
+
+        await Hotel.findByIdAndUpdate(req.params.hotelId, {
+            $push: { rooms: room._id }
+        });
+
+        res.status(201).json({ success: true, data: room });
+    } catch (err) {
+        if (err.name === 'ValidationError') {
+            const messages = Object.values(err.errors).map(e => e.message);
+            return res.status(400).json({ success: false, message: messages.join(', ') });
+        }
+        res.status(500).json({ success: false, message: err.message });
+    }
+};
+
 
 // @desc    update rooms
 // @route   PUT api/v1/hotels/:id/rooms/:id
@@ -42,13 +72,16 @@ exports.updateRoom = async (req, res) => {
         }
 
         // Make sure the room belongs to the hotel in the URL
-        if (room.hotelID.toString() !== req.params.hotelID) {
+        if (room.hotelID.toString() !== req.params.hotelId) {
             return res.status(400).json({ success: false, message: 'Room does not belong to this hotel' });
         }
 
         // hotelOwner can only edit rooms under their own hotel
-        if (req.user.role === 'hotelOwner' && room.hotelID.toString() !== req.user.hotelID?.toString()) {
-            return res.status(403).json({ success: false, message: 'Not authorized to edit this room' });
+        if (req.user.role === 'hotelOwner') {
+            const hotel = await Hotel.findById(room.hotelID);
+            if(hotel.ownerID.toString() !== req.user.id.toString()){
+                return res.status(403).json({ success: false, message: 'Not authorized to edit this room' });
+            }
         }
 
         // Prevent hotelID from being changed via update
@@ -77,3 +110,28 @@ exports.updateRoom = async (req, res) => {
 // @desc    delete rooms
 // @route   DELETE api/v1/hotels/:id/rooms/:id
 // @access  hotel
+
+exports.deleteRoom = async (req, res) => {
+    try {
+        const room = await Room.findById(req.params.id);
+        if (!room) {
+            return res.status(404).json({ success: false, message: 'Room not found' });
+        }
+
+        if (req.user.role === 'hotelOwner') {
+            const hotel = await Hotel.findById(room.hotelID);
+            if (!hotel || hotel.ownerID.toString() !== req.user.id.toString()) {
+                return res.status(403).json({ success: false, message: 'Not authorized to delete this room' });
+            }
+        }
+
+        await Hotel.findByIdAndUpdate(room.hotelID, {
+            $pull: { rooms: room._id }
+        });
+
+        await room.deleteOne();
+        res.status(200).json({ success: true, data: {} });
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
+};
